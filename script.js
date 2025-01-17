f43ef378996f1c538dfd39d9d930ab7a580c2800
FabioGomes79 <afabiogomes36@gmail.com>
2025-01-17 20:33:08 -0300

    :arrow_up: comecando o javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("add-item-form");
+const input = document.getElementById("item-input");
+const shoppingList = document.getElementById("shopping-list");
+const notification = document.getElementById("notification");
+const closeNotificationBtn = document.querySelector(".close-btn");
+
